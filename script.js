91df523e561ba16d342fcd8562e5f627ff31e6b3
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2025-07-31 17:18:31 +0000

    aula 31.07

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ let linguagens = ["JavaScript", "Python", "C++"];
 
 const item = document.querySelector("#lista");
 item.textContent = linguagens[2];
+
+
+let aluno = {
+    nome: "Fernanda",
+    idade: 37,
+    anoLetivo: "2ª Série",
+    MateriasFavoritas: ["Matemática", "Geografia", "História", "Arte", "Filosofia"]
+}
+console.log(aluno.nome);
+console.log(aluno.MateriasFavoritas[4])
+
+const objeto = document.querySelector("#alunos");
+objeto.textContent = aluno.nome
+objeto.textContent = aluno.idade
